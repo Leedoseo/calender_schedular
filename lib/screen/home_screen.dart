08c4dfb44e78b1f310b80aa9eb6b1bd45fb31e53
Leedoseo <okok0840@naver.com>
2025-05-19 16:36:11 +0900

    MainCalendar 위젯에 생성한 파라미터들을 HomeScreen에서 입력

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -2,20 +2,41 @@ import 'package:flutter/material.dart';
 import 'package:calendar_schedular/component/main_calendar.dart';
 
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget { // StatefulWidget으로 변경
   const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  DateTime selectedDate = DateTime.utc( // 선택된 날짜를 관리할 변수
+    DateTime.now().year,
+    DateTime.now().month,
+    DateTime.now().day,
+  );
 
   @override
 
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-          child: Column(
-            children: [
-              MainCalendar(),
+        child: Column(
+          children: [
+            MainCalendar(
+              selectedDate: selectedDate, // 선택된 날짜 전달
+              onDaySelected: onDaySelected, // 날짜가 선택됐을 때 실행할 함수
+              ),
             ],
           ),
       ),
     );
   }
+  void onDaySelected(DateTime selectedDate, DateTime focusedDate) {
+    setState(() {
+      this.selectedDate = selectedDate;
+    });
+  }
 }
+
