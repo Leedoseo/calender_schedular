ade94650092384d9dbf435b108595d53d610fdc7
Leedoseo <okok0840@naver.com>
2025-05-22 16:50:58 +0900

    키보드 높이 조정

--- lib/component/schedule_bottom_sheet.dart
+++ lib/component/schedule_bottom_sheet.dart
@@ -15,23 +15,28 @@ class _ScheduleBottomSheetState extends State<ScheduleBottomSheet> {
   @override
 
   Widget build(BuildContext context) {
+
+    final bottomInset = MediaQuery.of(context).viewInsets.bottom; // 키보드 높이 가져오기
+
     return SafeArea(
         child: Container(
-          height: MediaQuery.of(context).size.height / 2,
+          height: MediaQuery.of(context).size.height / 2 + bottomInset, // 화면 반 높이에 키보드 높이 추가하기
           color: Colors.white,
-          child: Column(
+          child: Padding(
+            padding: EdgeInsets.only(left: 8, right: 8, top: 8, bottom: bottomInset),
+          child: Column( // 시간 관련 텍스트 필드와 내용 관련 텍스트 필드 세로 배치
             children: [
-              Row(
+              Row( // 시작 시간, 종료 시간 가로로 배치
                 children: [
                   Expanded(
-                    child: CustomTextField(
+                    child: CustomTextField( // 시작 시간 입력 필드
                         label: "시작 시간",
                         isTime: true,
                     ),
                   ),
                   const SizedBox(width: 16.0),
                   Expanded(
-                    child: CustomTextField(
+                    child: CustomTextField( // 종료 시간 입력 필드
                         label: "종료 시간",
                         isTime: true,
                     ),
@@ -40,14 +45,14 @@ class _ScheduleBottomSheetState extends State<ScheduleBottomSheet> {
               ),
               SizedBox(height: 8.0),
               Expanded(
-                child: CustomTextField(
+                child: CustomTextField( // 내용 입력 필드
                     label: "내용",
                     isTime: false,
                 ),
               ),
               SizedBox(
                 width: double.infinity,
-                child: ElevatedButton(
+                child: ElevatedButton( // 저장 버튼
                   onPressed: onSavePressed,
                   style: ElevatedButton.styleFrom(
                     foregroundColor: PRIMARY_COLOR,
